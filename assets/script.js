910e92b4f2982e2b0b2a01945f30c18fd5c685c4
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-05-30 09:24:14 -0500

    API's working

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,19 @@
-
-
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid")
-    .then(response => response.json())
-    .then(data => {
-
-        console.log(data);
+fetch(
+    "http://api.openweathermap.org/geo/1.0/direct?q=New+Orleans&limit=5&appid=d6aac51f8f278c439b163e4e7767e456"
+  )
+    
+    .then((response) => response.json())
+    .then((citiesFound) => {
+      let firstCity = citiesFound[0];
+      console.log(firstCity.lat);
+      console.log(firstCity.lon);
+  
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=d6aac51f8f278c439b163e4e7767e456`
+      );
     })
-
-fetch("http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid")
-    .then(response => response.json())
-    .then(data => {
-
-        console.log(data);
-
-    })
+  
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
